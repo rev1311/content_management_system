b8a0bfc97d5ba748a4250dd6fde5f12034cc703d
Michael Partin <unmykx@gmmail.com>
2020-01-15 18:09:09 -0800

    fixed few errors in base structure

--- cms_db.sql
+++ cms_db.sql
@@ -4,14 +4,16 @@ USE cms_db;
 
 CREATE TABLE department (
 	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL
+	deptname VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
 	id INT NOT NULL AUTO_INCREMENT,
 	title VARCHAR(50) NOT NULL,
 	salary INT NOT NULL,
-
+    -- FOREIGN KEY(id) REFERENCES department(id),
+    PRIMARY KEY(id)
 );
 
 
@@ -19,6 +21,7 @@ CREATE TABLE employees (
 	id INT NOT NULL AUTO_INCREMENT,
 	firstname VARCHAR(20),
 	lastname VARCHAR(20),
-	FOREIGN KEY (id) FROM role(id),
+--     FOREIGN KEY(id) REFERENCES role(title),
+-- 	FOREIGN KEY (id) REFERENCES role(id),
 	PRIMARY KEY (id)
 );
