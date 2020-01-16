42db213797431cea913b115e6e1d664358d37839
Michael Partin <unmykx@gmmail.com>
2020-01-16 15:47:37 -0800

    nothing changed, working on joining/FK methods

--- cms_db.sql
+++ cms_db.sql
@@ -3,35 +3,35 @@ CREATE DATABASE cms_db;
 USE cms_db;
 
 CREATE TABLE department (
-	dept_id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
-    PRIMARY KEY(dept_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-	role_id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	title VARCHAR(50) NOT NULL,
 	salary INT NOT NULL,
-    FOREIGN KEY(dept_id) REFERENCES department(dept_id),
-    PRIMARY KEY(role_id)
+    PRIMARY KEY(id)
 );
-
 
 CREATE TABLE employees (
-	emp_id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	firstname VARCHAR(20),
 	lastname VARCHAR(20),
-    FOREIGN KEY(role_id) REFERENCES role,
-	FOREIGN KEY (manager_id) REFERENCES role,
-	PRIMARY KEY (emp_id)
+	role_id INT NOT NULL,
+	-- manager_id VARCHAR(20) NULL,
+	PRIMARY KEY (id)
 );
 
-INSERT INTO employees (firstname, lastname)
-VALUES ("Buck", "Doe"), ("Jane", "Houston"), ("Jim", "Hancock"), ("Will", "Black"),
-("Felicia", "Knight"), ("Ben", "Sharpie"), ("Bill", "Shakespeare");
+INSERT INTO employees (firstname, lastname, role_id)
+VALUES ("Buck", "Doe", 1), ("Jane", "Houston", 3), ("Jim", "Hancock", 3), ("Will", "Black", 2),
+("Felicia", "Knight", 2), ("Ben", "Sharpie", 3), ("Bill", "Shakespeare", 2);
 
 INSERT INTO role (title, salary)
-VALUES ("Manager", 150000), ("Engineer", 200000), ("Developer", 100000), ("Associate", 100000);
+VALUES ("Engineer", 200000), ("Developer", 100000), ("Associate", 100000);
 
 INSERT INTO department (name)
 VALUES ("Sales"), ("Production"), ("Admin")
+
+  
